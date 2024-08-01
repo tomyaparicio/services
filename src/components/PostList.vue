aceb969000d356b20d47821bf29280ff85c63a5f
Tomas Aparicio <tomyaparicio99@gmail.com>
2024-08-01 16:55:33 -0300

    service

---
+++ src/components/PostList.vue
@@ -0,0 +1,32 @@
+<template>
+ <h1>Listado Post</h1>
+ <ul class="post-list">
+    <li v-for="post in posts" v-bind:key="post.id">{{ post.title }}</li>
+ </ul>
+</template>
+
+<script lang="ts" setup>
+import PostServices from '@/services/PostService'; 
+import { defineComponent, onMounted } from 'vue';
+
+// export default defineComponent({
+//   name: "PostList",
+//   setup () {
+      const service = new PostServices();
+      const posts = service.getPost()
+
+      onMounted(async() => {
+          await service.fetchAll()
+      })
+    //   return {posts}
+//   }
+// })
+  
+
+</script>
+
+<style scoped>
+
+
+
+</style>
---
+++ src/services/PostService.ts
@@ -0,0 +1,29 @@
+import {ref} from 'vue'
+
+class PostServices {
+    private posts 
+
+    constructor() {
+            this.posts = ref([])
+    }
+
+    getPost () {
+        return this.posts
+    }
+
+    async fetchAll() {   
+        try {
+            const url = "https://jsonplaceholder.typicode.com/posts"
+            const response =  await fetch(url)
+            const json = await response.json()
+            this.posts.value = await json
+
+        } catch (error) {
+
+            console.log(error)
+        }
+    }
+
+}
+
+export default PostServices
